e072c389136fd04ee9143ab56f3a90ac167faca4
SHW\kornel.horvath <kornel.horvath@shiwaforce.com>
2016-10-25 16:03:54 +0200

    rewrite to plugin

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var LessHint = require('lesshint');
-module.exports = function(source) {
-    this.cacheable();
-    var lessHint = new LessHint(); 
+var glob = require("glob");
+function LessHintPlugin(options) {
+    this.options = options;
+}
+
+LessHintPlugin.prototype.apply = function (compiler) {
+    var lessHint = new LessHint();
     var defaultReporter = lessHint.getReporter();
-    defaultReporter.report(lessHint.checkString(source))
-    return source;
+    glob(this.options.files, function (err, files) {
+        if (err) {
+            throw new Error('Error processing files');
+        }
+        for (var i=0,iLen=files.length;i<iLen;i++) {
+            lessHint.checkFile(files[i]).then((report)=>{
+                defaultReporter.report(report);
+            });
+        }
+    });
 };
+
+module.exports = LessHintPlugin;
