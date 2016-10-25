83c7e078c5cd8dff8b57ef4071a870022cdadc0f
SHW\kornel.horvath <kornel.horvath@shiwaforce.com>
2016-10-25 16:35:13 +0200

    globs

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var LessHint = require('lesshint');
-var glob = require("glob");
-function LessHintPlugin(options) {
-    this.options = options;
+var glob = require("multi-glob").glob;
+var arrify = require('arrify');
+function LessHintPlugin(options, lessOptions) {
+    this.options = options || {};
+    this.lessOptions = lessOptions;
 }
 
 LessHintPlugin.prototype.apply = function (compiler) {
     var lessHint = new LessHint();
+    lessHint.configure(this.lessOptions);
+    //var defaultReporter = lessHint.getReporter(require('lesshint-reporter-stylish'));
     var defaultReporter = lessHint.getReporter();
-    glob(this.options.files, function (err, files) {
+    glob(arrify(this.options.files || ''), function (err, files) {
         if (err) {
             throw new Error('Error processing files');
         }
-        for (var i=0,iLen=files.length;i<iLen;i++) {
-            lessHint.checkFile(files[i]).then((report)=>{
+        for (var i = 0, iLen = files.length; i < iLen; i++) {
+            lessHint.checkFile(files[i]).then((report) => {
                 defaultReporter.report(report);
             });
         }
