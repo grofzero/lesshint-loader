51a2edaad926cb7f9dace2f70d1c0821c8a1d8ce
Kornél Horváth <kornel.horvath@shiwaforce.com>
2016-10-25 21:43:29 +0200

    read config location from options

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var LessHint = require('lesshint');
 var glob = require('glob-all');
 var arrify = require('arrify');
-function LessHintPlugin(options, lessOptions) {
+function LessHintPlugin(options) {
     this.options = options || {};
-    this.lessOptions = lessOptions;
+    this.lessOptions = options.configFile?  require(options.configFile): {}; 
 }
 
 LessHintPlugin.prototype.apply = function (compiler) {
     var lessHint = new LessHint();
     lessHint.configure(this.lessOptions);
-    var reporter = require('lesshint-reporter-stylish');
-    //var defaultReporter = lessHint.getReporter();
-    glob(this.options.files || '', function (err, files) {
+    var reporter = this.options.reporter || require('lesshint-reporter-stylish');
+    glob(this.options.files || [], function (err, files) {
         if (err) {
             throw new Error('Error processing files');
         }
