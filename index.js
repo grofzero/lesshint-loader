06ab4e867802cb6d8a9b757cc0d66325d31622ae
Kornél Horváth <kornel.horvath@shiwaforce.com>
2016-10-25 19:26:28 +0200

    colored output

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ function LessHintPlugin(options, lessOptions) {
 LessHintPlugin.prototype.apply = function (compiler) {
     var lessHint = new LessHint();
     lessHint.configure(this.lessOptions);
-    //var defaultReporter = lessHint.getReporter(require('lesshint-reporter-stylish'));
-    var defaultReporter = lessHint.getReporter();
+    var reporter = require('lesshint-reporter-stylish');
+    //var defaultReporter = lessHint.getReporter();
     glob(this.options.files || '', function (err, files) {
         if (err) {
             throw new Error('Error processing files');
         }
         for (var i = 0, iLen = files.length; i < iLen; i++) {
             lessHint.checkFile(files[i]).then((report) => {
-                defaultReporter.report(report);
+                reporter.report(report);
             });
         }
     });
