c7a10a4d1a8699ef6d41f176c4db8e0ec7f1c1be
SHW\kornel.horvath <kornel.horvath@shiwaforce.com>
2016-12-22 14:41:26 +0100

    Lint on every build

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ var LessHint = require('lesshint');
 var glob = require('glob-all');
 var arrify = require('arrify');
 function LessHintPlugin(options) {
-    this.options = options || {};
-    this.lessOptions = options.configFile?  require(options.configFile): {}; 
+	this.options = options || {};
+	this.lessOptions = options.configFile ? require(options.configFile) : {};
 }
 
 LessHintPlugin.prototype.apply = function (compiler) {
-    var lessHint = new LessHint();
-    lessHint.configure(this.lessOptions);
-    var reporter = this.options.reporter || require('lesshint-reporter-stylish');
-    glob(this.options.files || [], function (err, files) {
-        if (err) {
-            throw new Error('Error processing files');
-        }
-        for (var i = 0, iLen = files.length; i < iLen; i++) {
-            lessHint.checkFile(files[i]).then((report) => {
-                reporter.report(report);
-            });
-        }
-    });
+	var self = this;
+	compiler.plugin('done', function () {
+		var lessHint = new LessHint();
+		lessHint.configure(self.lessOptions);
+		var reporter = self.options.reporter || require('lesshint-reporter-stylish');
+		glob(self.options.files || [], function (err, files) {
+			if (err) {
+				throw new Error('Error processing files');
+			}
+			for (var i = 0, iLen = files.length; i < iLen; i++) {
+				lessHint.checkFile(files[i]).then((report) => {
+					reporter.report(report);
+				});
+			}
+		});
+	});
 };
 
 module.exports = LessHintPlugin;
